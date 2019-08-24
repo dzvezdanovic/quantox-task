e8e005fef4b829fd679a10672db7349d050d9ece
Darko <darkoinkzvezdanovic@gmail.com>
2019-08-25 00:17:18 +0200

    search page

---
+++ app/search.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <title>Quantox Task</title>
+  <link
+      href="http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600"
+      rel="stylesheet"
+      type="text/css"
+  />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">  <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<div class="container">
+<div class="row">
+  <table class="table table-bordered">
+    <thead>
+    <tr>
+      <th>Id</th>
+      <th>Username</th>
+      <th>Email</th>
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Username </td>
+            <td>email@temp.com </td>
+        </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+</body>
+</html>
+
